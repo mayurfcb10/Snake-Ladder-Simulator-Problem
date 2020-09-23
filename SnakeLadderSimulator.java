9e8d916c0e7da9a90fb8b1574997586fcd131c5d
Mayur <mayurzope555@gmail.com>
2020-09-23 22:39:29 +0530

    [Mayur]Added: Game played with Single Player at Start Position 0

--- SnakeLadderSimulator.java
+++ SnakeLadderSimulator.java
@@ -1,7 +1,12 @@
 /* Welcome to the Snake Ladder Simulator Problem */
 
-public class SnakeLadderSimulator{
-	public static void main(String[] args){
+public class SnakeLadderSimulator {
+	public static void main(String[] args) {
 		System.out.println("Welcome to the Snake Ladder Simulator Problem");
+		int startPosition = (int)(Math.random() * 10) % 2;
+
+		if(startPosition == 0) {
+			System.out.println("The start position for player is "+startPosition);
+		}	
 	}
 } 
