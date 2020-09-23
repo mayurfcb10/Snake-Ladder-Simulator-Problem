24b89bfa2c3b7bed81c91973be0c1275ac336470
Mayur <mayurzope555@gmail.com>
2020-09-23 23:09:01 +0530

    [Mayur]Added: Check for the possible option to move the Player's position

--- SnakeLadderSimulator.java
+++ SnakeLadderSimulator.java
@@ -1,19 +1,27 @@
 /* Welcome to the Snake Ladder Simulator Problem */
 
 public class SnakeLadderSimulator {
-	public static final int safePosition = 0;
+	public static final int noPlay = 0;
 	public static final int snake = 1;
 	public static final int ladder = 2;
 	public static final int finalPosition = 100;
-	public static final int initialPosition = 0 ;
-	
-	private static int getRandomInteger(int max, int min) {
-		return (int)(Math.random()*(max - min))+ min ;
-	}
+	public static int initialPosition = 0;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Snake Ladder Simulator Problem");
-		System.out.println("Player rolls the Die the outcome is "+getRandomInteger(6, 1));
-
+		int position = 0;
+		int option = (int) (Math.random()* 10) % 3;
+		System.out.println("Selected Option is " + option);
+		int dieOutcome = (int)(Math.random()*(6 - 1))+ 1 ;
+		System.out.println("The Die Outcome is : " + dieOutcome);
+		if(option == snake) {
+			position -= dieOutcome ; 
+		}else if(option == ladder) {
+			position += dieOutcome ;
+		}else {
+			position += 0;
+		}
+		System.out.println("Position after one step " + position);
 	}
 
 }
