0f9f73236ed0a44876ebf19cbffc0dd808d9d855
Mayur <mayurzope555@gmail.com>
2020-09-23 23:51:56 +0530

    [Mayur]Added: 2 - Players game reporting the player who reached the destination first or Winner

--- SnakeLadderSimulator.java
+++ SnakeLadderSimulator.java
@@ -1,40 +1,67 @@
 /* Welcome to the Snake Ladder Simulator Problem */
 
-public class SnakeLadderSimulator {
+public class UC7_2_Player_and_Winner {
+
 	public static final int noPlay = 0;
 	public static final int snake = 1;
 	public static final int ladder = 2;
 	public static final int finalPosition = 100;
-	public static int initialPosition = 0;
+	public static final int initialPosition = 0;
+	public static final int Player1 = 1;
+	public static final int Player2 = 2;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Snake Ladder Simulator Problem");
-		int position = 0;
-		int countDiceRoll = 0;
-		while(position >= 0 && position != finalPosition) {
+		int position1 = initialPosition, position2 = 0;
+		int selectPlayer = (int)(Math.random() * 10 ) % 2 + 1;
+
+		while(position1 >= 0 && position1 != finalPosition && position2 >= 0 && position2 != finalPosition ) {
 			int option = (int) (Math.random()* 10) % 3;
-			System.out.println(option);
 			int dieOutcome = (int)(Math.random()*(6 - 1))+ 1 ;
-			countDiceRoll++;
-			System.out.println(dieOutcome);
-			if(option == snake) {
-				position -= dieOutcome ; 
-				if(position < 0) {
-					position = 0;
+			switch (selectPlayer) {
+			case Player1:
+				if(option == snake) {
+					position1 -= dieOutcome ; 
+					if(position1 < 0) {
+						position1 = 0;
+					}
+				}else if(option == ladder) {
+					position1 += dieOutcome ;
+					if(position1 > 100) {
+						position1 -= dieOutcome;
+					}
+				}else {
+					position1 += 0;
 				}
-			}else if(option == ladder) {
-				position += dieOutcome ;
-				if(position > 100) {
-					position -= dieOutcome;
+			case Player2:
+				if(option == snake) {
+					position2 -= dieOutcome ; 
+					if(position2 < 0) {
+						position2 = 0;
+					}
+				}else if(option == ladder) {
+					position2 += dieOutcome ;
+					if(position2 > 100) {
+						position2 -= dieOutcome;
+					}
+				}else {
+					position2 += 0;
 				}
-			}else {
-				position += 0;
+
+
 			}
-			System.out.println("Position at "+countDiceRoll+" DieRoll is "+position);
 		}
-		System.out.println("Total Number of time dice rolled is "+countDiceRoll);
+
+		if(position1 == finalPosition) {
+			System.out.println("Winner is Position1 at destination "+position1);
+		}else if(position2 == finalPosition){
+			System.out.println("Winner is Position2 at destination "+position2);
+		}
 
 	}
 
-
 }
+
+
+
+
